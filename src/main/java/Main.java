f187053460c6d392f683a9b4dad8a4e81aee6173
Николай Прядкин <nikolai.priadkin@gmail.com>
2020-04-28 16:41:56 +0300

    add react

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -17,13 +19,22 @@ public class Main {
         server.addConnector(connector);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
+        context.setContextPath("/test");
 
         context.addServlet(new ServletHolder(new TestConnection()), "/test");
 
+        ResourceHandler rh = new ResourceHandler();
+        rh.setCacheControl("public, max-age=86400");
+
+        ContextHandler contextHtml= new ContextHandler();
+        contextHtml.setContextPath("/");
+        contextHtml.setResourceBase(Main.class.getClassLoader().getResource("html").toExternalForm());
+        contextHtml.setHandler(rh);
+
+
         ContextHandlerCollection contexts = new ContextHandlerCollection();
 
-        contexts.setHandlers(new Handler[]{context});
+        contexts.setHandlers(new Handler[]{context, contextHtml});
 
         server.setHandler(contexts);
 
